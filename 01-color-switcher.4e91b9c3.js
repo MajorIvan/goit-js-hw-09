3f36bf69db8531d1a6ac76d5564c0724694e863c
MajorIvan <118841894+MajorIvan@users.noreply.github.com>
2023-11-25 15:59:41 +0000

    Deploying to gh-pages from @ MajorIvan/goit-js-hw-09@39f3d1f562a14721ff4b27dabb68f8092eccfa92 🚀

---
+++ 01-color-switcher.4e91b9c3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.style.padding="8px 20px",e.style.padding="8px 20px";document.querySelector("div").style.textAlign="center";let d=null;e.disabled=!0,t.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4e91b9c3.js.map
