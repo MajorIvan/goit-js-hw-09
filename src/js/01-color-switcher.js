e8f9e649d264e6f2ddc26d04b178bac31a068e4e
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-25 17:53:01 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const div = document.querySelector('div');
+div.style.textAlign= 'center';
+startBtn.style.padding = '8px 20px';
+stopBtn.style.padding = '8px 20px';
+
+
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
